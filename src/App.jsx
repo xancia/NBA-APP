f4e5e6bb6ea5f8c383e94afebf4ea7ceba5642ec
Xancia <cminhma@gmail.com>
2023-12-15 10:18:02 -0500

    small change

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
 
   return (
-    <div>
+    <div className="">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/teams" element={<Teams />} />
