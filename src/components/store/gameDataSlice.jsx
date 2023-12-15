42c0d740b548b1f6720006d36ca2edaef2147395
Xancia <cminhma@gmail.com>
2023-12-15 10:35:50 -0500

    added logos

---
+++ src/components/store/gameDataSlice.jsx
@@ -0,0 +1,14 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+const gameDataSlice = createSlice({
+    name: 'gameData',
+    initialState: [],
+    reducers: {
+       
+    }
+})
+
+// export const {  } = gameDataSlice.actions
+
+export default gameDataSlice.reducer
