0e964537d024d6c64d4c26a4d628601394f72b73
Xancia <cminhma@gmail.com>
2023-12-15 15:08:07 -0500

    styling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,10 +4,10 @@ import Nav from "./Nav"
 const Home = () => {
 
   return (
-    <div>
+    <div className=" bg-gradient-to-b from-gray-600 to bg-slate-800">
       <Nav />
       <div className="flex justify-center items-center h-screen pb-20">
-      <iframe width="1080" height="620" src="https://www.youtube.com/embed/XPeT2Tfk9gM?si=7xlLKE6ea_did5zb" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <iframe width="1080" height="620" src="https://www.youtube.com/embed/XPeT2Tfk9gM?si=7xlLKE6ea_did5zb" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
       </div>
     </div>
   )
